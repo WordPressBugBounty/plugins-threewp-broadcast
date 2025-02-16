e50e4e599d8af4d28829e45f4704822f817f31f8
Sean Murphy <sean@iamseanmurphy.com>
2025-02-16 20:18:27 +0000

    51.03

--- threewp-broadcast/ThreeWP_Broadcast.php
+++ threewp-broadcast/ThreeWP_Broadcast.php
@@ -7,10 +7,10 @@ Description:	Broadcast / multipost posts, with attachments, custom fields and ta
 Domain Path:	/lang
 Plugin Name:	Broadcast
 Plugin URI:		https://broadcast.plainviewplugins.com/
-Version:		51.02
+Version:		51.03
 */
 
-DEFINE( 'THREEWP_BROADCAST_VERSION', 51.02 );
+DEFINE( 'THREEWP_BROADCAST_VERSION', 51.03 );
 
 require_once( __DIR__ . '/vendor/autoload.php' );
 
